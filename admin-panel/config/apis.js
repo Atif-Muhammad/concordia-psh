c4cbf8abcedd89fd2bcf64fbb99f450b209c7620
Atif-Muhammad <mohatif02@gmail.com>
2026-09-14 22:31:10 +0500

    m

--- admin-panel/config/apis.js
+++ admin-panel/config/apis.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { format } from "date-fns";
 import { formatLocalDate } from "../src/lib/utils";
 
-// const base_url = "http://localhost:3003/api";
-const base_url = "https://beams.hayatfoundation.org.pk/api";
+const base_url = "http://localhost:3003/api";
+// const base_url = "https://beams.hayatfoundation.org.pk/api";
 
 export const userWho = async () => {
   try {
